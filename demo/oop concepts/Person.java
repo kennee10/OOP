2fba15d8278a2c90bca8663ba16d5af1b3e329b0
kennee10 <kennee100@gmail.com>
2022-01-18 15:27:56 +0800

    2

--- demo/oop concepts/Person.java
+++ demo/oop concepts/Person.java
@@ -1,14 +1,29 @@
 public class Person {
     // instance attributes
-    double weight;
-    double height;
-    int age;
+    private double weight;
+    private double height;
+    private int age;
+    // String name;
 
     // can define as many constructors but only 1 will run
     // specific constructor
-    public Person(double weight, double height, int age) {
+    public void Person(double weight, double height, int age) {
         this.weight = weight; // if no this. prefix, it will refer to the parameter
-        this.height = height;
+        this.height = height; // local variables
         this.age = age;
+    }
+
+    public double calculateBMI() {
+        return this.weight/(height * height);
+    }
+
+    // accessor, getter
+    public double getWeight() {
+        return weight;
+    }
+
+    // setter,
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 }
--- demo/oop concepts/Test.java
+++ demo/oop concepts/Test.java
@@ -1,9 +1,20 @@
 public class Test {
     public static void main(String[] args) {
-        Person john = new Person(50, 1.5, 15);
+        // Person john = new Person(50, 1.5, 15);
 
-        System.out.println(john.weight);
-        System.out.println(john.height);
-        System.out.println(john.age);
+        // System.out.println(john.weight);
+        // System.out.println(john.height);
+        // System.out.println(john.age);
+
+        // Person mary = new Person(60, 1.7, 15);
+        Person mary;
+        // System.out.println(mary.name);
+
+        // System.out.println(john.calculateBMI());
+        // System.out.println(mary.calculateBMI());
+
+        // System.out.println(john.getWeight());
+        // john.setWeight(100);
+        // System.out.println(john.getWeight());
     }
 }
