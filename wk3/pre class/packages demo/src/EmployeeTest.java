c2d1e616e6d0bcb1d63bcb6fee192d08861721fc
kennee10 <kennee100@gmail.com>
2022-01-25 16:00:30 +0800

    3

---
+++ wk3/pre class/packages demo/src/EmployeeTest.java
@@ -0,0 +1,8 @@
+import entity.*;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee e = new Employee("Joe");
+        System.out.println(e.getName());
+    }
+}
