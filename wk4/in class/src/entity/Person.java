e9268803df14dbad9b03ab4c0856e2ae694704c5
kennee10 <kecheong.2019@sis.smu.edu.sg>
2022-01-29 14:20:05 +0800

    4

---
+++ wk4/in class/src/entity/Person.java
@@ -0,0 +1,16 @@
+// if I own smu.edu.sg
+// package convension -> sg.edu.smu.<projName>
+package entity;
+
+public class Person {
+    private int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    
+    public String toString() {
+        return "" + age;
+    }
+}
---
+++ wk4/in class/src/test/PersonTest.java
@@ -0,0 +1,8 @@
+// package test;
+import entity.Person;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        System.out.println(new Person(12));
+    }
+}
