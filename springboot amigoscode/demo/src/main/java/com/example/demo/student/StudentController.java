4b47629c30db5139fbe8912104eb30394174b673
kennee10 <kennee100@gmail.com>
2022-03-06 22:44:01 +0800

    tut

--- springboot amigoscode/demo/src/main/java/com/example/demo/student/StudentController.java
+++ springboot amigoscode/demo/src/main/java/com/example/demo/student/StudentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,11 @@ public class StudentController {
     @GetMapping // the only REST endpoint we have
 	public List<Student> getStudents(){
 		return studentService.getStudents();
+	}
+
+	@PostMapping
+	public void registerNewStudent(@RequestBody Student student) {
+		studentService.addNewStudent(student);
 	}
 }
 
